cbd1e88f99af585030a741157f28a40bd842ee48
Dannee Kim <dannee.kim.dev@gmail.com>
2023-05-12 10:01:59 -0400

    Retrieve API data via HTTP GET request, minor error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import https from "node:https";
 
+interface Holiday {
+  name: string;
+  date: string;
+}
+
 const baseUrl = "https://date.nager.at/api/v3";
 const countryCode = "AU";
 const year = 2023;
@@ -7,23 +12,31 @@ const year = 2023;
 function getPublicHolidays(
   countryCode: string,
   year: number = new Date().getFullYear()
-) {
+): Promise<Holiday[]> {
   const url = `${baseUrl}/PublicHolidays/${year}/${countryCode}`;
 
-  https
-    .get(url, (res) => {
-      console.log(`status: ${res.statusCode}`);
-      console.log(`headers:`, res.headers, "\n");
-
-      res.on("data", (d) => {
-        process.stdout.write(d);
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+          data += chunk;
+        });
+        response.on("end", () => {
+          try {
+            const holidays: Holiday[] = JSON.parse(data);
+            resolve(holidays);
+            // console.log(holidays);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on("error", (e) => {
+        console.error(e);
+        reject(e);
       });
-
-      res.on("end", () => {});
-    })
-    .on("error", (e) => {
-      console.error(e);
-    });
+  });
 }
 
-getPublicHolidays("AU");
+const holidays = getPublicHolidays("AU");
