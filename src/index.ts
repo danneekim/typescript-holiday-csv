bff8c09ff95cc2c6dfd1efd9eca17693dc8bf198
Dannee Kim <dannee.kim.dev@gmail.com>
2023-05-12 09:42:44 -0400

    WIP: getPublicHoliday() - logging api data to console

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-export function sayHello() {
-  console.log("Hello World");
+import https from "node:https";
+
+const baseUrl = "https://date.nager.at/api/v3";
+const countryCode = "AU";
+const year = 2023;
+
+function getPublicHolidays(
+  countryCode: string,
+  year: number = new Date().getFullYear()
+) {
+  const url = `${baseUrl}/PublicHolidays/${year}/${countryCode}`;
+
+  https
+    .get(url, (res) => {
+      console.log(`status: ${res.statusCode}`);
+      console.log(`headers:`, res.headers, "\n");
+
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+
+      res.on("end", () => {});
+    })
+    .on("error", (e) => {
+      console.error(e);
+    });
 }
-sayHello();
+
+getPublicHolidays("AU");
